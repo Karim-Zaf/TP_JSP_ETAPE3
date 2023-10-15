d56e027210fa545656a82121965cda28ad11197e
marmotton <bouchkhaf@gmail.com>
2023-10-15 18:06:07 +0200

    Sujet etudiant pour le TP3 JSP MVC

--- src/main/java/fr/devavance/metier/beans/User.java
+++ src/main/java/fr/devavance/metier/beans/User.java
@@ -6,13 +6,4 @@ package fr.devavance.metier.beans;
  */
 public class User {
 
-    // properties
-    private String userName = "";  // Nom d'utilisateur
-    private String password = "";  // Mot de passe
-    private String profil = "";  // Profil de l'utilisateur "admin" ou "user"
-    private Boolean auth = false; // Important : déconnecté par défaut
-
-    
-
-
 }
