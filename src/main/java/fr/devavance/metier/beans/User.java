c94a8874c4ee89f9bed80001f4b83e9b9c7bdcdf
bl <bouchkhaf@gmail.com>
2025-02-02 12:01:30 +0100

    Ajout des attributs de la classe User

--- src/main/java/fr/devavance/metier/beans/User.java
+++ src/main/java/fr/devavance/metier/beans/User.java
@@ -5,5 +5,9 @@ package fr.devavance.metier.beans;
  * @author bouchaib.lemaire
  */
 public class User {
+    private String userName = "";  // Nom d'utilisateur
+    private String password = "";  // Mot de passe
+    private String profil = "";  // Profil de l'utilisateur "admin" ou "user"
+    private Boolean auth = false; // Important : déconnecté par défaut
 
 }
