29422c95f91f55d71cd2c84de0810cbe6218f4ad
bl <bouchkhaf@gmail.com>
2025-02-11 17:26:52 +0100

    avancement sur la question 2

---
+++ src/main/java/fr/devavance/metier/beans/Seance.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.devavance.metier.beans;
+
+/**
+ *
+ * @author kn_10
+ */
+public class Seance {
+    
+}
